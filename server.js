535ee6965aaa1c759db8ff882ec2f661e221ec7b
AZNJAWA <gamekuakun726@gmail.com>
2026-09-25 02:27:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const { createClient } = require("@libsql/client");

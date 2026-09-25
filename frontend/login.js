6cd44f3fce37f0a3b119a1b16fb2c272fa57efc7
AZNJAWA <gamekuakun726@gmail.com>
2026-09-25 07:22:45 +0700

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,31 +1,37 @@
 const loginForm = document.getElementById("loginForm");
 
+console.log("LOGIN JS BERJALAN");
+console.log("FORM:", loginForm);
+
 loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    console.log("SUBMIT LOGIN:", email);
+
     try {
         const response = await fetch("/login", {
             method: "POST",
-
             headers: {
                 "Content-Type": "application/json"
             },
-
             body: JSON.stringify({
                 email: email,
                 password: password
             })
         });
 
+        console.log("STATUS:", response.status);
+
         const data = await response.json();
+
+        console.log("RESPONSE:", data);
 
         if (data.berhasil) {
             console.log("Login berhasil:", data);
             window.location.href = "/chat.html";
-            
         } else {
             console.log("Login gagal:", data.pesan);
         }
